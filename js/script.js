ff88fdf75364a7a65285273e8f91f1bb6354ed2d
yeon44 <jangyeon33@gmail.com>
2024-10-05 11:35:55 +0000

    1번 완료

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 let ex_01 = prompt("세자리 이상의 정수를 입력하세요.", "");
-if( ex_01 % 4 == 0 ){
+if (ex_01 % 4 == 0) {
   document.write(ex_01, "은 4의 배수입니다");
-} else (ex_01, "은 4의 배수가 아닙니다");
+} else {
+  document.write(ex_01, "은 4의 배수가 아닙니다");
+}
