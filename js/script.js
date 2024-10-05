67bf9cfe8ec7ae4dd59e99a0977b30e2941bf4cb
yeon44 <jangyeon33@gmail.com>
2024-10-05 11:50:15 +0000

    2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,28 @@
-let ex_01 = prompt("세자리 이상의 정수를 입력하세요.", "");
-if (ex_01 % 4 == 0) {
-  document.write(ex_01, "은 4의 배수입니다");
-} else {
-  document.write(ex_01, "은 4의 배수가 아닙니다");
-}
+// let ex_01 = prompt("세자리 이상의 정수를 입력하세요.", "");
+// if (ex_01 % 4 == 0) {
+//   document.write(ex_01, "은 4의 배수입니다");
+// } else {
+//   document.write(ex_01, "은 4의 배수가 아닙니다");
+// }
+
+// let ex_02 = prompt("이름을 입력하세요", "");
+// let score = prompt("점수를 입력하세요", "");
+// if (score >= 95 && score <= 100) {
+//   document.write(ex_02, " 학생은 A+등급 입니다");
+// } else if (score >= 90 && score <= 95) {
+//   document.write(ex_02, " 학생은 A등급 입니다");
+// } else if (score >= 85 && score <= 90) {
+//   document.write(ex_02, " 학생은 B+등급 입니다");
+// } else if (score >= 80 && score <= 85) {
+//   document.write(ex_02, " 학생은 B등급 입니다");
+// } else if (score >= 75 && score <= 80) {
+//   document.write(ex_02, " 학생은 C+등급 입니다");
+// } else if (score >= 70 && score <= 75) {
+//   document.write(ex_02, " 학생은 C등급 입니다");
+// } else if (score >= 65 && score <= 70) {
+//   document.write(ex_02, " 학생은 D+등급 입니다");
+// } else if (score >= 60 && score <= 65) {
+//   document.write(ex_02, " 학생은 D등급 입니다");
+// } else {
+//   document.write(ex_02 + " 학생은 F입니다");
+// }
